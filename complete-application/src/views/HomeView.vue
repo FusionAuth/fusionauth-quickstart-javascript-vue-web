b6906e7b92192863f447244fa7681419f71f7b9b
Bri Davidson <bri@madebychalk.com>
2024-04-01 09:38:38 -0600

    10 Add Redirect Handling to Log In or Create New Account CTA

--- complete-application/src/views/HomeView.vue
+++ complete-application/src/views/HomeView.vue
@@ -1,9 +1,19 @@
+
 <template>
   <div class="column-container">
     <div class="content-container">
       <div style="margin-bottom: 100px;">
         <h1>Welcome to Changebank</h1>
-        <p>To get started, <a style="cursor: pointer">log in or create a new account</a>.</p>
+        <p>
+            To get started,
+            <button className="button-redirect" style = "cursor: pointer"  @click="fusionAuth.login()">
+            log in
+            </button>
+            or
+            <button className="button-redirect" style = "cursor: pointer"  @click="fusionAuth.register()">
+            create a new account.
+            </button>
+          </p>
       </div>
     </div>
 
@@ -12,3 +22,10 @@
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import {useFusionAuth} from "@fusionauth/vue-sdk";
+
+const fusionAuth = useFusionAuth();
+
+</script>
