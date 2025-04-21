3d3675cd0127c820fe5c939e42c153c4f0db4026
Mark Robustelli <137117976+mark-robustelli@users.noreply.github.com>
2025-04-21 09:08:40 -0700

    update app id

--- complete-application/src/main.ts
+++ complete-application/src/main.ts
@@ -9,7 +9,7 @@ import FusionAuthVuePlugin from '@fusionauth/vue-sdk';
 const app = createApp(App)
 
 app.use(FusionAuthVuePlugin, {
-  clientId: 'e9fdb985-9173-4e01-9d73-ac2d60d1dc8e',
+  clientId: 'ec526002-35cc-4e6e-8f5b-0e4fba2b08c8',
   serverUrl: 'http://localhost:9011',
   redirectUri: 'http://localhost:5173',
   postLogoutRedirectUri: 'http://localhost:5173/logged-out',
